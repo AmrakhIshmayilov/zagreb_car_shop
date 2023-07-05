89861a7d2bf1e3ea5d7bc01f6fc85d905f87be43
gabilgg07 <gabilgg07@gmail.com>
2023-07-05 05:37:42 +0400

    Trying to use Revalution slider pluginç but failed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from "react";
 import "./home.css";
+import MainSlider from "../MainSlider/MainSlider";
 
 const Home = () => {
   useEffect(() => {
     document.title = "Zagreb";
   }, []);
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <MainSlider />
+    </div>
+  );
 };
 
 export default Home;
